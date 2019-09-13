0d1ffad28d792a02eb5e80a00b61d27445d2c83a
Denis Ojdanic <denis.ojdanic@yahoo.com>
2019-09-13 10:06:29 +0200

    update on install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ rm master.zip
 
 
 echo
-echo "Downloading MSDGen ..."
+echo "Downloading MDCStream ..."
 echo
 wget https://github.com/CN-TU/mdcstream-matlab/archive/master.zip
 unzip master.zip
@@ -23,17 +23,3 @@ echo "Download complete!"
 echo
 
 
-echo
-echo "Running Tests for MDCGen ..."
-echo
-matlab -nodisplay -nosplash -nodesktop -r "run('mdcgen-matlab-master/runUnitTests.m');exit;" | tail -n +11
-
-
-echo
-echo "Running Tests for MDCStream ..."
-echo
-matlab -nodisplay -nosplash -nodesktop -r "run('mdcstream-matlab-master/runUnitTests.m');exit;" | tail -n +11
-
-echo
-echo "Installation complete!"
-echo
